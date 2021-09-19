15b1ed48f4755407fb431449dd1e530cf7285c5a
aloisdg <aloisdegouvello@live.fr>
2021-09-19 10:24:50 +0200

    refactor(main): use semantic html

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -12,22 +12,22 @@ const Main = (props: IMainProps) => (
     {props.meta}
 
     <div className="max-w-screen-md mx-2 sm:mx-4 md:mx-auto">
-      <div className="border-b border-gray-700">
+      <header className="border-b border-gray-700">
         <div className="pt-16 pb-8">
-          <div className="font-bold text-3xl text-white">{AppConfig.title}</div>
-          <div className="text-xl text-white">{AppConfig.description}</div>
+          <h1 className="font-bold text-3xl text-white">{AppConfig.title}</h1>
+          <h2 className="text-xl text-white">{AppConfig.description}</h2>
         </div>
-      </div>
+      </header>
 
       <div className="py-5 text-xl content">{props.children}</div>
 
-      <div className="border-t border-gray-700 text-center py-8 text-sm text-white">
+      <footer className="border-t border-gray-700 text-center py-8 text-sm text-white">
         © Copyright {new Date().getFullYear()} {AppConfig.title}. Powered with{' '}
         <span role="img" aria-label="Love">
           ♥
         </span>{' '}
         by <a href="https://aloisdg.netlify.app">aloisdg</a>
-      </div>
+      </footer>
     </div>
   </div>
 );
